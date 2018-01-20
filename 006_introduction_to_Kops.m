469ec99d863dd816d05f60249cc6df82ea21e755
Gopikrishna <a.gopikrishna01@gmail.com>
2018-01-20 13:20:25 +0530

    Create new file

---
+++ 006_introduction_to_Kops.m
@@ -0,0 +1,13 @@
+# Introduction to Kops (Kuberneties Operations)
+
++ To setup Kuberneties on AWS, We can use a tool called Kops
+  - Kops stands for Kuberneties Operations
++ Kops allows us to production grade Kuberneties installations, upgrades and management
+  - Using Kops we can start a cluster on AWS 
++ There is also a legacy tool called kube-up.sh
+  - This is a simple tool to bring up a cluster, But it does not create a production ready enviornment
++ Kops only works on Linux/Mac
+  - To run Kops on windows we need to boot up a linux box
++ Using vagrant we can bootup a Linux box
+
++ We can download Virtualbox from https://www.virtualbox.org and Vagrant from https://www.vagrantup.com/downloads.html (we need both).
